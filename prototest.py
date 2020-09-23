93f67fc1c2a26da95e9f5e6617fc0762a9fead8e
potter <4692149776@qq.com>
2020-09-23 23:13:16 +0800

    协议页面初版

--- prototest.py
+++ prototest.py
@@ -1,13 +1,28 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,url_for,redirect
 import config
 
 app = Flask(__name__)
 app.config.from_object(config)
 
-@app.route("/")
-def hello_world():
 
-    return render_template("index.html")
+@app.route("/", methods=["GET","POST"])
+def hello_world():
+    if request.method == "GET":
+        return render_template("index.html")
+    else:
+        ip = request.form.get("login_ip")
+        account = request.form.get("account")
+        print(ip, account)
+        return redirect(url_for("proto_test"))
+
+
+@app.route("/proto/", methods=["GET","POST"])
+def proto_test():
+    if request.method == "GET":
+        return render_template("proto.html")
+    else:
+        pass
+
 
 
 if __name__ == "__main__":
